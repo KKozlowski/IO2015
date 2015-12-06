4ca01310afcd7193e99eb2599180b87c0231402e
LukaszSobczyk <lukasz.sobczyk@poczta.onet.pl>
2015-12-06 15:44:17 +0100

    Implementacja certyfikatu - zmiana nazwy metody edit na set by trzymać się konwencji javy

--- src/crew/Certificate.java
+++ src/crew/Certificate.java
@@ -1,30 +1,55 @@
 package crew;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Date;
 
 public class Certificate {
-
-	private String name;
-
-	private List<SkillType> skills;
-
-	private Date expirationDate;
-
+	
 	private int certificateID;
-
-	private SkillType skillType;
-
-	public void removeSkill() {
-
+	private String name;
+	private List<SkillType> skills;
+	private Date expirationDate;
+	
+	
+	public Certificate(){
+		certificateID=0;
+		name=null;
+		skills = new ArrayList<SkillType>();
+		expirationDate = null;
 	}
-
-	public void addSkill() {
-
+	public Certificate(int ID, String designation){
+		certificateID=ID;
+		name=designation;
+		skills = new ArrayList<SkillType>();
+		expirationDate = null;
 	}
-
-	public void editName() {
-
+	
+	public int getCertificateID() {
+		return certificateID;
 	}
-
+	public void setCertificateID(int certificateID) {
+		this.certificateID = certificateID;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<SkillType> getSkills() {
+		return skills;
+	}
+	public void addSkill(SkillType skill) {
+		this.skills.add(skill);
+	}
+	public void removeSkill(int index) {
+		this.skills.remove(index);
+	}
+	public Date getExpirationDate() {
+		return expirationDate;
+	}
+	public void setExpirationDate(Date expirationDate) {
+		this.expirationDate = expirationDate;
+	}
 }
