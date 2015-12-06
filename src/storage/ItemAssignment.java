44e6f05305f41151bec1b93e3bc588b0f0eec206
Dracyon <dracyon@gmail.com>
2015-12-06 17:30:50 +0100

    Storage: adding getters, setters, constructors, implements some basic functions update part1, some further implementation

--- src/storage/ItemAssignment.java
+++ src/storage/ItemAssignment.java
@@ -5,7 +5,16 @@ import services.Service;
 
 public class ItemAssignment {
 
-	private Date biginning;
+	public ItemAssignment(Date beginning, Date end, Boolean serviceRent, Service service, SingleItem singleItem) {
+		super();
+		this.beginning = beginning;
+		this.end = end;
+		this.serviceRent = serviceRent;
+		this.service = service;
+		this.singleItem = singleItem;
+	}
+
+	private Date beginning;
 
 	private Date end;
 
@@ -14,5 +23,45 @@ public class ItemAssignment {
 	private Service service;
 
 	private SingleItem singleItem;
+
+	public Date getBeginning() {
+		return beginning;
+	}
+
+	public void setBeginning(Date beginning) {
+		this.beginning = beginning;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public Boolean getServiceRent() {
+		return serviceRent;
+	}
+
+	public void setServiceRent(Boolean serviceRent) {
+		this.serviceRent = serviceRent;
+	}
+
+	public Service getService() {
+		return service;
+	}
+
+	public void setService(Service service) {
+		this.service = service;
+	}
+
+	public SingleItem getSingleItem() {
+		return singleItem;
+	}
+
+	public void setSingleItem(SingleItem singleItem) {
+		this.singleItem = singleItem;
+	}
 
 }
--- src/storage/Storage.java
+++ src/storage/Storage.java
@@ -1,30 +1,63 @@
 package storage;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
 public class Storage {
 
-	private List<SingleItem> Amount;
+	public Storage(String name) {
+		super();
+		Name = name;
+	}
+
+	
+
+	private ArrayList<SingleItem> Amount = new ArrayList<SingleItem>();
 
 	private String Name;
 
 	private SingleItem[] singleItem;
 
-	public Void removeObject(SingleItem SingleObject) {
-		return null;
+	public void removeObject(int i) {
+		Amount.remove(i);
 	}
 
-	public Void showStorage() {
-		return null;
+	public void showStorage() {
+	
+		for (int i = 0; i<Amount.size(); i++){
+			System.out.println(Amount.get(i));
+		}
 	}
 
-	public Void addObject(SingleItem SingleObject) {
-		return null;
+	public void addObject(SingleItem SingleObject) {
+		Amount.add(SingleObject);
 	}
 
 	public Void modificateObject(SingleItem SingleObject) {
 		return null;
+	}
+
+	public String getName() {
+		return Name;
+	}
+
+	public void setName(String name) {
+		Name = name;
+	}
+
+	public SingleItem[] getSingleItem() {
+		return singleItem;
+	}
+
+	public void setSingleItem(SingleItem[] singleItem) {
+		this.singleItem = singleItem;
+	}
+
+	@Override
+	public String toString() {
+		return "Storage [Amount=" + Amount + ", Name=" + Name + ", singleItem=" + Arrays.toString(singleItem) + "]";
 	}
 
 }
