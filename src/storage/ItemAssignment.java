9261e7ec2c0156d65ddf05d61528887f9132f028
Dracyon <dracyon@gmail.com>
2015-12-06 17:30:50 +0100

    Storage: adding getters, setters, constructors, implements some basic functions

--- src/storage/ItemAssignment.java
+++ src/storage/ItemAssignment.java
@@ -5,7 +5,7 @@ import services.Service;
 
 public class ItemAssignment {
 
-	private Date biginning;
+	private Date beginning;
 
 	private Date end;
 
@@ -14,5 +14,45 @@ public class ItemAssignment {
 	private Service service;
 
 	private SingleItem singleItem;
+
+	public Date getBeginning() {
+		return beginning;
+	}
+
+	public void setBeginning(Date beginning) {
+		this.beginning = beginning;
+	}
+
+	public Date getEnd() {
+		return end;
+	}
+
+	public void setEnd(Date end) {
+		this.end = end;
+	}
+
+	public Boolean getServiceRent() {
+		return serviceRent;
+	}
+
+	public void setServiceRent(Boolean serviceRent) {
+		this.serviceRent = serviceRent;
+	}
+
+	public Service getService() {
+		return service;
+	}
+
+	public void setService(Service service) {
+		this.service = service;
+	}
+
+	public SingleItem getSingleItem() {
+		return singleItem;
+	}
+
+	public void setSingleItem(SingleItem singleItem) {
+		this.singleItem = singleItem;
+	}
 
 }
--- src/storage/Ship.java
+++ src/storage/Ship.java
@@ -5,6 +5,27 @@ import java.util.List;
 
 public class Ship extends SingleItem {
 
+	public Ship(String startName, boolean startAvailable, String startDescription, ItemType startItemType) {
+		super(startName, startAvailable, startDescription, startItemType);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<String> getDocumentsNeeded() {
+		return DocumentsNeeded;
+	}
+
+	public void setDocumentsNeeded(List<String> documentsNeeded) {
+		DocumentsNeeded = documentsNeeded;
+	}
+
+	public int getCapacity() {
+		return Capacity;
+	}
+
+	public void setCapacity(int capacity) {
+		Capacity = capacity;
+	}
+
 	private int Capacity;
 
 	private List<String> DocumentsNeeded;
